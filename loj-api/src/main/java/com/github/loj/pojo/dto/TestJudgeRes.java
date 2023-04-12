ff9b9bf483faa3db08d7d8355c91f9271bb9041a
lxhcaicai <2778763221@qq.com>
2023-04-13 01:37:05 +0800

    [add] 添加TestJudge请求处理返回类

---
+++ loj-api/src/main/java/com/github/loj/pojo/dto/TestJudgeRes.java
@@ -0,0 +1,60 @@
+package com.github.loj.pojo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/13 1:22
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestJudgeRes implements Serializable {
+    private static final long serialVersionUID = 222L;
+
+    /**
+     *  评测结果状态码
+     */
+    private Integer status;
+
+    /**
+     *  评测运行时间消耗 ms
+     */
+    private Long time;
+
+    /**
+     *  评测运行空间消耗 kb
+     */
+    private Long memory;
+
+    /**
+     * 输入
+     */
+    private String Input;
+
+    /**
+     * 期望输出
+     */
+    private String expectedOutput;
+
+    /**
+     * 运行标准输出
+     */
+    private String stdout;
+
+    /**
+     * 运行错误输出
+     */
+    private String stderr;
+
+    /**
+     * 原题的评测模式：default、spj、interactive
+     */
+    private String problemJudgeMode;
+}
