3413a31fc6ef880ca3f496165d0d9261edf407f7
lxhcaicai <2778763221@qq.com>
2023-04-13 01:20:51 +0800

    add JudgeController

---
+++ loj-judge/src/main/java/com/github/loj/controller/JudgeController.java
@@ -0,0 +1,12 @@
+package com.github.loj.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/13 1:19
+ */
+@RestController
+public class JudgeController {
+
+}
