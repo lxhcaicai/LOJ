876e6e7bf1013049e9283bb6875d9828bddc17e6
lxhcaicai <2778763221@qq.com>
2023-04-13 00:19:13 +0800

    搭建spring cloud 环境 注册nacos配置中心服务发现

---
+++ loj-judge/src/main/java/com/github/loj/JudgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.loj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JudgeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeServerApplication.class, args);
+    }
+}
