89a020904e3016d7ba1e858906cd876c1a7941b1
lxhcaicai <2778763221@qq.com>
2023-04-14 00:36:03 +0800

    [add] /test-judge controller

--- loj-judge/src/main/java/com/github/loj/controller/JudgeController.java
+++ loj-judge/src/main/java/com/github/loj/controller/JudgeController.java
@@ -1,5 +1,11 @@
 package com.github.loj.controller;
 
+import com.github.loj.common.CommonResult;
+import com.github.loj.pojo.dto.TestJudgeReq;
+import com.github.loj.pojo.dto.TestJudgeRes;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JudgeController {
 
+    @PostMapping(value = "/test-judge")
+    public CommonResult<TestJudgeRes> submitProblemTestJudge(@RequestBody TestJudgeReq testJudgeReq) {
+
+        if(testJudgeReq == null
+                || StringUtils.isEmpty(testJudgeReq.getCode())
+                || StringUtils.isEmpty(testJudgeReq.getLanguage())
+                || StringUtils.isEmpty(testJudgeReq.getUniqueKey())
+                || testJudgeReq.getTimeLimit() == null
+                || testJudgeReq.getMemoryLimit() == null
+                || testJudgeReq.getStackLimit() == null) {
+            return CommonResult.errorResponse("调用参数错误!请检查你的调用参数!");
+        }
+        return CommonResult.successResponse();
+    }
 }
