cda988616ec8701911a160c5b27c871b2831c8c8
lxhcaicai <2778763221@qq.com>
2023-04-15 19:39:30 +0800

    [add] 沙箱信号表示

--- loj-judge/src/main/java/com/github/loj/judge/SandboxRun.java
+++ loj-judge/src/main/java/com/github/loj/judge/SandboxRun.java
@@ -15,6 +15,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -73,6 +74,41 @@ public class SandboxRun {
     public static String getSandboxBaseUrl() {
         return SANDBOX_BASE_URL;
     }
+
+    public static final List<String> signals = Arrays.asList(
+            "", // 0
+            "Hangup", // 1
+            "Interrupt", // 2
+            "Quit", // 3
+            "Illegal instruction", // 4
+            "Trace/breakpoint trap", // 5
+            "Aborted", // 6
+            "Bus error", // 7
+            "Floating point exception", // 8
+            "Killed", // 9
+            "User defined signal 1", // 10
+            "Segmentation fault", // 11
+            "User defined signal 2", // 12
+            "Broken pipe", // 13
+            "Alarm clock", // 14
+            "Terminated", // 15
+            "Stack fault", // 16
+            "Child exited", // 17
+            "Continued", // 18
+            "Stopped (signal)", // 19
+            "Stopped", // 20
+            "Stopped (tty input)", // 21
+            "Stopped (tty output)", // 22
+            "Urgent I/O condition", // 23
+            "CPU time limit exceeded", // 24
+            "File size limit exceeded", // 25
+            "Virtual timer expired", // 26
+            "Profiling timer expired", // 27
+            "Window changed", // 28
+            "I/O possible", // 29
+            "Power failure", // 30
+            "Bad system call" // 31
+    );
 
     public JSONArray run(String uri, JSONObject param) throws SystemError {
         HttpHeaders headers = new HttpHeaders();
