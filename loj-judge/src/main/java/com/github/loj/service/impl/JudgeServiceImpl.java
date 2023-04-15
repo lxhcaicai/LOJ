59a96ef371659076e42212f17a7d8f9903900c47
lxhcaicai <2778763221@qq.com>
2023-04-15 21:20:20 +0800

    [add] 完善 JudgeService testJudge方法

--- loj-judge/src/main/java/com/github/loj/service/impl/JudgeServiceImpl.java
+++ loj-judge/src/main/java/com/github/loj/service/impl/JudgeServiceImpl.java
@@ -1,16 +1,23 @@
 package com.github.loj.service.impl;
 
+import com.github.loj.judge.JudgeContext;
 import com.github.loj.pojo.dto.TestJudgeReq;
 import com.github.loj.pojo.dto.TestJudgeRes;
 import com.github.loj.service.JudgeService;
+
+import javax.annotation.Resource;
 
 /**
  * @author lxhcaicai
  * @date 2023/4/14 0:40
  */
 public class JudgeServiceImpl implements JudgeService {
+
+    @Resource
+    private JudgeContext judgeContext;
+
     @Override
     public TestJudgeRes testJudge(TestJudgeReq testJudgeReq) {
-        return null;
+        return judgeContext.testJudgeRes(testJudgeReq);
     }
 }
