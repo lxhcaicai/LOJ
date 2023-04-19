2a4363a8a417123db131d7851ba0611dd2d1f7eb
lxhcaicai <2778763221@qq.com>
2023-04-20 00:56:14 +0800

    [add] 添加Jedis 配置

---
+++ loj-backup/src/main/java/com/github/loj/config/JedisPoolConfigure.java
@@ -0,0 +1,17 @@
+package com.github.loj.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/20 0:54
+ */
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "loj.redis")
+@Data
+public class JedisPoolConfigure {
+}
