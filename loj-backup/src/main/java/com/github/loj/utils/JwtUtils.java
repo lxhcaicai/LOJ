8832b467514ca8272a252d41c2fd72a6bed1ed6c
lxhcaicai <2778763221@qq.com>
2023-04-21 00:42:38 +0800

    [add] 添加 Jwt Utils

---
+++ loj-backup/src/main/java/com/github/loj/utils/JwtUtils.java
@@ -0,0 +1,85 @@
+package com.github.loj.utils;
+
+import com.github.loj.shiro.ShiroConstant;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/20 22:59
+ */
+@Slf4j(topic = "loj")
+@Data
+@Component
+@ConfigurationProperties(prefix = "loj.jwt")
+public class JwtUtils {
+    private String secret;
+
+    private long expire;
+
+    private String header;
+
+    private long checkRefreshExpire;
+
+    @Autowired RedisUtils redisUtils;
+
+    /**
+     * 生成jwt token
+     * @param userId
+     * @return
+     */
+    public String generateToken(String userId) {
+        Date nowDate = new Date();
+        // 过期时间
+        Date expireDate = new Date(nowDate.getTime() + expire * 1000);
+
+        String token = Jwts.builder()
+                .setHeaderParam("type", "JWT")
+                .setSubject(userId)
+                .setIssuedAt(nowDate)
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+
+        redisUtils.set(ShiroConstant.SHIRO_TOKEN_KEY + userId, token, expire);
+        redisUtils.set(ShiroConstant.SHIRO_TOKEN_REFRESH + userId, "1", checkRefreshExpire);
+        return token;
+    }
+
+    public Claims getClaimByToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            log.debug("validate is token error ", e);
+            return null;
+        }
+    }
+
+    public void cleanToken(String uid) {
+        redisUtils.del(ShiroConstant.SHIRO_TOKEN_KEY + uid, ShiroConstant.SHIRO_TOKEN_REFRESH + uid);
+    }
+
+    public boolean hasToken(String uid) {
+        return redisUtils.hasKey(ShiroConstant.SHIRO_TOKEN_KEY + uid);
+    }
+
+    /**
+     * token是否过期
+     *
+     * @return true：过期
+     */
+    public boolean isTokenExpire(Date expiration) {
+        return expiration.before(new Date());
+    }
+}
