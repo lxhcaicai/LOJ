60f8e48883b7c71da2096bd9bba02a934ed4eb0e
lxhcaicai <2778763221@qq.com>
2023-04-21 00:42:06 +0800

    [add] 添加ShiroConstant

---
+++ loj-backup/src/main/java/com/github/loj/shiro/ShiroConstant.java
@@ -0,0 +1,16 @@
+package com.github.loj.shiro;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/21 0:31
+ */
+public class ShiroConstant {
+
+    public final static String SHIRO_TOKEN_KEY = "shiro:token-key:uid:";
+
+    public final static String SHIRO_TOKEN_REFRESH = "shiro:token-refresh:uid:";
+
+    public final static String SHIRO_AUTHORIZATION_CACHE = "shiro:authorization:uid:";
+
+    public final static String SHIRO_TOKEN_LOCK = "shiro:token-lock:uid:";
+}
