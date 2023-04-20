be8c348e8c81b6088d4a3ac27bf311fcd25210dc
lxhcaicai <2778763221@qq.com>
2023-04-21 01:49:12 +0800

    [add] AccountProfile 存在redis session的当前登录用户信息

---
+++ loj-backup/src/main/java/com/github/loj/shiro/AccountProfile.java
@@ -0,0 +1,44 @@
+package com.github.loj.shiro;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/21 1:45
+ * 存在redis session的当前登录用户信息
+ */
+@Data
+public class AccountProfile implements Serializable {
+
+    @ApiModelProperty(value = "用户id")
+    private String uid;
+
+    @ApiModelProperty(value = "用户名")
+    private String username;
+
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "真实姓名")
+    private String realname;
+
+    @ApiModelProperty(value = "头衔名称")
+    private String titleName;
+
+    @ApiModelProperty(value = "头衔背景颜色")
+    private String titleColor;
+
+    @ApiModelProperty(value = "头像地址")
+    private String avatar;
+
+    @ApiModelProperty(value = "0可用，1不可用")
+    private int status;
+
+    public String getId() { //shiro登录用户实体默认主键获取方法要为getId
+        return uid;
+    }
+}
