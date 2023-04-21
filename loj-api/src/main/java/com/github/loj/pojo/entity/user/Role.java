bd261ca15c56daf8257cd28d2ce3e8e01f9e0b2a
lxhcaicai <2778763221@qq.com>
2023-04-22 02:52:55 +0800

    [add] Role 角色实体类

--- loj-api/src/main/java/com/github/loj/pojo/entity/user/Role.java
+++ loj-api/src/main/java/com/github/loj/pojo/entity/user/Role.java
@@ -4,7 +4,11 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -13,6 +17,10 @@ import java.util.Date;
  * @author lxhcaicai
  * @date 2023/4/22 2:47
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Role对象", description="")
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
