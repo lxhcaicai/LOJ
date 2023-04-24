6081fa848bed079b702260748a71e33d20c4d5d3
lxhcaicai <2778763221@qq.com>
2023-04-24 21:19:57 +0800

    [add] SysMsgVO 用户的系统消息

---
+++ loj-backup/src/main/java/com/github/loj/pojo/vo/SysMsgVO.java
@@ -0,0 +1,35 @@
+package com.github.loj.pojo.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/24 21:14
+ */
+@ApiModel(value="用户的系统消息", description="")
+@Data
+public class SysMsgVO {
+
+    private Long id;
+
+    @ApiModelProperty(value = "通知标题")
+    private String title;
+
+    @ApiModelProperty(value = "通知内容")
+    private String content;
+
+    @ApiModelProperty(value = "发布通知的管理员id")
+    private String adminId;
+
+    @ApiModelProperty(value = "消息类型, 系统通知Sys,我的信息Mine")
+    private String type;
+
+    @ApiModelProperty(value = "是否已读")
+    private Boolean status;
+
+    private Date gmtCreate;
+}
