47acae4ce24d6eb6cb00a6b822a8bb4ce13105f9
lxhcaicai <2778763221@qq.com>
2023-04-25 00:15:36 +0800

    [add] StatusAccessDeniedException

---
+++ loj-backup/src/main/java/com/github/loj/common/exception/StatusAccessDeniedException.java
@@ -0,0 +1,26 @@
+package com.github.loj.common.exception;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/25 0:15
+ */
+public class StatusAccessDeniedException extends Exception{
+    public StatusAccessDeniedException() {
+    }
+
+    public StatusAccessDeniedException(String message) {
+        super(message);
+    }
+
+    public StatusAccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StatusAccessDeniedException(Throwable cause) {
+        super(cause);
+    }
+
+    public StatusAccessDeniedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
