c092666669aa2dd26a732786e75f6be9ecd78a0f
lxhcaicai <2778763221@qq.com>
2023-04-25 00:40:29 +0800

    [add] MybatisPlusConfig 配置类

---
+++ loj-backup/src/main/java/com/github/loj/config/MybatisPlusConfig.java
@@ -0,0 +1,37 @@
+package com.github.loj.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/25 0:37
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.github.loj.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 注册乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
