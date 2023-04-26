f97dbf4033b79e8892ffa4d9c1776ebb5f407c33
lxhcaicai <2778763221@qq.com>
2023-04-27 01:50:14 +0800

    [add] tomcat 配置类

---
+++ loj-backup/src/main/java/com/github/loj/config/TomcatConfig.java
@@ -0,0 +1,29 @@
+package com.github.loj.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/27 1:48
+ * 允许请求路径出现[] \ 等特殊字符
+ */
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+                connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+                connector.setProperty("rejectIllegalHeader", "false");
+            }
+        });
+        return factory;
+    }
+}
