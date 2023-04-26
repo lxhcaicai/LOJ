3d1ff17cb804a52ab24c1668f4d4a2db35b18873
lxhcaicai <2778763221@qq.com>
2023-04-26 23:20:47 +0800

    [add] 判题的异步线程池 配置类

---
+++ loj-backup/src/main/java/com/github/loj/config/JudgeAsyncTaskConfig.java
@@ -0,0 +1,39 @@
+package com.github.loj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/26 23:15
+ * 判题的异步线程池
+ */
+@Configuration
+@EnableAsync
+public class JudgeAsyncTaskConfig {
+
+    @Bean
+    public Executor judgeTaskAsyncPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //核心线程池大小
+        executor.setCorePoolSize(2);
+        //最大线程数
+        executor.setMaxPoolSize(10);
+        //队列容量
+        executor.setQueueCapacity(500);
+        //活跃时间
+        executor.setKeepAliveSeconds(3);
+        //线程名字前缀
+        executor.setThreadNamePrefix("JudgeExecutor-");
+        // setRejectedExecutionHandler：当pool已经达到max size的时候，如何处理新任务
+        // CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
