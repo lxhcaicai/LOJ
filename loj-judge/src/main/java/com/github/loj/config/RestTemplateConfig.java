b923d3bd1d582e1f6be812e7fe9f39758c6f5948
lxhcaicai <2778763221@qq.com>
2023-04-28 23:47:00 +0800

    [add] RestTemplateConfig

---
+++ loj-judge/src/main/java/com/github/loj/config/RestTemplateConfig.java
@@ -0,0 +1,28 @@
+package com.github.loj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/28 23:43
+ */
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(90000); // 单位ms
+        factory.setConnectTimeout(10000); // 单位ms
+        return factory;
+    }
+
+}
