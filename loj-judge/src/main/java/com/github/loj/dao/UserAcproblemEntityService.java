c62cac55428567cb3268f8be88fdc98dea9a550d
lxhcaicai <2778763221@qq.com>
2023-04-29 23:06:39 +0800

    [add] UserAcproblemEntityService

---
+++ loj-judge/src/main/java/com/github/loj/dao/UserAcproblemEntityService.java
@@ -0,0 +1,11 @@
+package com.github.loj.dao;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.loj.pojo.user.UserAcproblem;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/29 22:55
+ */
+public interface UserAcproblemEntityService extends IService<UserAcproblem> {
+}
---
+++ loj-judge/src/main/java/com/github/loj/dao/impl/UserAcproblemEntityServiceImpl.java
@@ -0,0 +1,15 @@
+package com.github.loj.dao.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.loj.dao.UserAcproblemEntityService;
+import com.github.loj.mapper.UserAcproblemMapper;
+import com.github.loj.pojo.user.UserAcproblem;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/29 23:03
+ */
+@Service
+public class UserAcproblemEntityServiceImpl extends ServiceImpl<UserAcproblemMapper, UserAcproblem> implements UserAcproblemEntityService {
+}
