b174283a58cb0da8bf5f3eb902f98ce27f23a93a
lxhcaicai <2778763221@qq.com>
2023-04-29 23:07:44 +0800

    [add] UserAcproblemMapper

---
+++ loj-judge/src/main/java/com/github/loj/mapper/UserAcproblemMapper.java
@@ -0,0 +1,15 @@
+package com.github.loj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.loj.pojo.user.UserAcproblem;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/29 23:05
+ */
+@Mapper
+@Repository
+public interface UserAcproblemMapper extends BaseMapper<UserAcproblem> {
+}
