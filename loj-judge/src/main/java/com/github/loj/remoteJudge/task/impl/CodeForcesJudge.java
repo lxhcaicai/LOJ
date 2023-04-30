42a3de5476c3ffaf6c475c609bf4d4431c871732
lxhcaicai <2778763221@qq.com>
2023-04-30 13:12:31 +0800

    [add] CodeForcesJudge

---
+++ loj-judge/src/main/java/com/github/loj/remoteJudge/task/impl/CodeForcesJudge.java
@@ -0,0 +1,30 @@
+package com.github.loj.remoteJudge.task.impl;
+
+import com.github.loj.remoteJudge.entity.RemoteJudgeRes;
+import com.github.loj.remoteJudge.task.RemoteJudgeStrategy;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/4/30 13:11
+ */
+public class CodeForcesJudge extends RemoteJudgeStrategy {
+    @Override
+    public void submit() {
+
+    }
+
+    @Override
+    public RemoteJudgeRes result() {
+        return null;
+    }
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public String getLanguage(String language) {
+        return null;
+    }
+}
