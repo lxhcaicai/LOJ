c4d55a18e7105324ece355eba18ce3c9ed3ada0f
lxhcaicai <2778763221@qq.com>
2023-04-30 09:20:54 +0800

    [add] add method updateOtherTable(...) in JudgeContest

--- loj-judge/src/main/java/com/github/loj/judge/JudgeContext.java
+++ loj-judge/src/main/java/com/github/loj/judge/JudgeContext.java
@@ -1,9 +1,13 @@
 package com.github.loj.judge;
 
 import com.github.loj.common.exception.SystemError;
+import com.github.loj.dao.ContestRecordEntityService;
+import com.github.loj.dao.UserAcproblemEntityService;
 import com.github.loj.judge.entity.LanguageConfig;
 import com.github.loj.pojo.dto.TestJudgeReq;
 import com.github.loj.pojo.dto.TestJudgeRes;
+import com.github.loj.pojo.user.UserAcproblem;
+import com.github.loj.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +24,14 @@ public class JudgeContext {
     @Autowired
     private JudgeStrategy judgeStrategy;
 
+    @Autowired
+    private UserAcproblemEntityService userAcproblemEntityService;
+
     @Resource
     private LanguageConfigLoader languageConfigLoader;
+
+    @Resource
+    private ContestRecordEntityService contestRecordEntityService;
 
     public TestJudgeRes testJudgeRes(TestJudgeReq testJudgeReq) {
         // c和c++为一倍时间和空间，其它语言为2倍时间和空间
@@ -41,5 +51,28 @@ public class JudgeContext {
 
     public Boolean compileInteractive(String code, Long pid, String interactiveLanguage, HashMap<String, String> extraFiles) throws SystemError{
         return Compiler.compileInteractive(code, pid, interactiveLanguage, extraFiles);
+    }
+
+    public void updateOtherTable(Long submitId,
+                                 Integer status,
+                                 Long cid,
+                                 String uid,
+                                 Long pid,
+                                 Long gid,
+                                 Integer score,
+                                 Integer userTime) {
+        if(cid == 0) {
+            // 非比赛提交
+            // 如果是AC,就更新user_acproblem表,
+
+            if(status.intValue() == Constants.Judge.STATUS_ACCEPTED.getStatus() && gid == null) {
+                userAcproblemEntityService.saveOrUpdate(new UserAcproblem()
+                        .setPid(pid)
+                        .setUid(uid)
+                        .setSubmitId(submitId));
+            }
+        } else { // 如果时比赛提交
+            contestRecordEntityService.updateContestRecord(score, status, submitId, userTime);
+        }
     }
 }
