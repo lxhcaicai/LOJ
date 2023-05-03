ed79ef7f54eb7a59043b153623a65493dc171534
lxhcaicai <2778763221@qq.com>
2023-05-03 23:40:48 +0800

    [add] add method (judge) in JudgeService

--- loj-judge/src/main/java/com/github/loj/service/JudgeService.java
+++ loj-judge/src/main/java/com/github/loj/service/JudgeService.java
@@ -4,6 +4,7 @@ import com.github.loj.common.exception.SystemError;
 import com.github.loj.pojo.dto.TestJudgeReq;
 import com.github.loj.pojo.dto.TestJudgeRes;
 import com.github.loj.pojo.dto.ToJudgeDTO;
+import com.github.loj.pojo.entity.judge.Judge;
 
 import java.util.HashMap;
 
@@ -16,4 +17,6 @@ public interface JudgeService {
     public Boolean compileInteractive(String code, Long pid, String interactiveLanguage, HashMap<String,String> extraFiles) throws SystemError;
 
     public void remoteJudge(ToJudgeDTO toJudgeDTO);
+
+    public void judge(Judge judge);
 }
--- loj-judge/src/main/java/com/github/loj/service/impl/JudgeServiceImpl.java
+++ loj-judge/src/main/java/com/github/loj/service/impl/JudgeServiceImpl.java
@@ -1,23 +1,28 @@
 package com.github.loj.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.github.loj.common.exception.SystemError;
 import com.github.loj.dao.JudgeEntityService;
 import com.github.loj.dao.JudgeServerEntityService;
+import com.github.loj.dao.ProblemEntityService;
 import com.github.loj.judge.JudgeContext;
 import com.github.loj.pojo.dto.TestJudgeReq;
 import com.github.loj.pojo.dto.TestJudgeRes;
 import com.github.loj.pojo.dto.ToJudgeDTO;
 import com.github.loj.pojo.entity.judge.Judge;
+import com.github.loj.pojo.entity.problem.Problem;
 import com.github.loj.remoteJudge.RemoteJudgeContext;
 import com.github.loj.service.JudgeService;
 import com.github.loj.util.Constants;
+import org.apache.tomcat.util.bcel.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * @author lxhcaicai
@@ -37,6 +42,9 @@ public class JudgeServiceImpl implements JudgeService {
 
     @Autowired
     private RemoteJudgeContext remoteJudgeContext;
+
+    @Resource
+    private ProblemEntityService problemEntityService;
 
     @Override
     public TestJudgeRes testJudge(TestJudgeReq testJudgeReq) {
@@ -78,5 +86,68 @@ public class JudgeServiceImpl implements JudgeService {
         }
         remoteJudgeContext.judge(toJudgeDTO);
 
+    }
+
+    @Override
+    public void judge(Judge judge) {
+        // 标志该判题过程进入编译阶段
+        // 写入当前判题服务的名字
+        UpdateWrapper<Judge> judgeUpdateWrapper = new UpdateWrapper<>();
+        judgeUpdateWrapper.set("status", Constants.Judge.STATUS_COMPILING.getStatus())
+                .set("judger", name)
+                .eq("submit_id", judge.getSubmitId())
+                .ne("status", Constants.Judge.STATUS_CANCELLED.getStatus());
+        boolean isUpdateOk = judgeEntityService.update(judgeUpdateWrapper);
+
+        // 没更新成功，则可能表示该评测被取消 或者 judge记录被删除了，则结束评测
+        if(!isUpdateOk) {
+            judgeContext.updateOtherTable(judge.getSubmitId(),
+                    Constants.Judge.STATUS_CANCELLED.getStatus(),
+                    judge.getCid(),
+                    judge.getUid(),
+                    judge.getPid(),
+                    judge.getGid(),
+                    null,
+                    null);
+            return;
+        }
+
+        // 进行判题操作
+        QueryWrapper<Problem> problemQueryWrapper = new QueryWrapper<>();
+        problemQueryWrapper.select("id",
+                        "type",
+                        "io_score",
+                        "difficulty",
+                        "judge_mode",
+                        "judge_case_mode",
+                        "time_limit",
+                        "memory_limit",
+                        "stack_limit",
+                        "user_extra_file",
+                        "judge_extra_file",
+                        "case_version",
+                        "spj_code",
+                        "spj_language",
+                        "problem_id",
+                        "is_remove_end_blank")
+                .eq("id", judge.getPid());
+
+        Problem problem = problemEntityService.getOne(problemQueryWrapper);
+        Judge finalJudgeRes = judgeContext.Judge(problem,judge);
+
+        // 更新该次提交
+        judgeEntityService.updateById(finalJudgeRes);
+
+        if(!Objects.equals(finalJudgeRes.getStatus(), Constants.Judge.STATUS_SUBMITTED_FAILED.getStatus())) {
+            // 更新其它表
+            judgeContext.updateOtherTable(finalJudgeRes.getSubmitId(),
+                    finalJudgeRes.getStatus(),
+                    judge.getCid(),
+                    judge.getUid(),
+                    judge.getPid(),
+                    judge.getGid(),
+                    finalJudgeRes.getScore(),
+                    finalJudgeRes.getTime());
+        }
     }
 }
