977b56e96b8cbdb4793998cf2b705eecb6883c59
lxhcaicai <2778763221@qq.com>
2023-05-05 20:24:55 +0800

    [add] UserRecord对象

---
+++ loj-api/src/main/java/com/github/loj/pojo/entity/user/UserRecord.java
@@ -0,0 +1,45 @@
+package com.github.loj.pojo.entity.user;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/5 20:21
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value = "UserRecord对象", description = "")
+public class UserRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "用户id")
+    private String uid;
+
+    @ApiModelProperty(value = "cf得分")
+    @TableField("rating")
+    private Integer rating;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+}
