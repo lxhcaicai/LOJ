d5aac24cc3af7460e5ef9bf7fe5d6efd89bc9f9e
lxhcaicai <2778763221@qq.com>
2023-05-07 02:56:48 +0800

    [add] JudgeReceiver 测试调试部分

---
+++ loj-backup/src/main/java/com/github/loj/judge/self/JudgeReceiver.java
@@ -0,0 +1,57 @@
+package com.github.loj.judge.self;
+
+import cn.hutool.json.JSONUtil;
+import com.github.loj.judge.AbstractReceiver;
+import com.github.loj.judge.Dispatcher;
+import com.github.loj.pojo.dto.TestJudgeReq;
+import com.github.loj.utils.Constants;
+import com.github.loj.utils.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/7 1:31
+ */
+@Component
+@Slf4j(topic = "loj")
+public class JudgeReceiver extends AbstractReceiver {
+
+    @Autowired
+    private Dispatcher dispatcher;
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Async()
+    public void processWaitingTask() {
+        // 优先处理比赛的提交任务
+        // 其次处理普通提交的提交任务
+        // 最后处理在线调试的任务
+        handleWaitingTask(Constants.Queue.CONTEST_JUDGE_WAITING.getName(),
+                Constants.Queue.GENERAL_JUDGE_WAITING.getName(),
+                Constants.Queue.TEST_JUDGE_WAITING.getName());
+    }
+
+    @Override
+    public String getTaskByRedis(String queue) {
+        long size = redisUtils.lGetListSize(queue);
+        if(size > 0) {
+            return (String) redisUtils.lrPop(queue);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void handleJudgeMsg(String taskStr, String queueName) {
+        if(Constants.Queue.TEST_JUDGE_WAITING.getName().equals(queueName)) {
+            TestJudgeReq testJudgeReq = JSONUtil.toBean(taskStr, TestJudgeReq.class);
+            dispatcher.dispatch(Constants.TaskType.TEST_JUDGE, testJudgeReq);
+        } else {
+            // TODO 判题测
+        }
+    }
+}
