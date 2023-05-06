2b68a346c0731be83406592367c6b2cd5b2e44d8
lxhcaicai <2778763221@qq.com>
2023-05-07 00:51:24 +0800

    [add] 题目标签

---
+++ loj-api/src/main/java/com/github/loj/pojo/entity/problem/Tag.java
@@ -0,0 +1,50 @@
+package com.github.loj.pojo.entity.problem;
+
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/7 0:47
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Tag对象", description="")
+public class Tag implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "标签名字")
+    private String name;
+
+    @ApiModelProperty(value = "标签颜色")
+    private String color;
+
+    @ApiModelProperty(value = "标签所属oj")
+    private String oj;
+
+    @ApiModelProperty(value = "团队ID")
+    private Long gid;
+
+    @ApiModelProperty(value = "标签分类ID")
+    @TableField(updateStrategy = FieldStrategy.IGNORED)
+    private long tcid;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+}
