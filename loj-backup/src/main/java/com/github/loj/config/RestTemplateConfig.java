13a07198e81f6ab59eea2989f3c71e3e5489a5b4
lxhcaicai <2778763221@qq.com>
2023-05-07 02:54:28 +0800

    [add] RestTemplateConfig 配置类

---
+++ loj-backup/src/main/java/com/github/loj/config/RestTemplateConfig.java
@@ -0,0 +1,27 @@
+package com.github.loj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/7 2:52
+ */
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(600000); //单位为ms
+        factory.setConnectTimeout(50000); //单位为ms
+        return factory;
+    }
+}
