d6bce04b48d7e2b7815ef20ae274c2d364151a0f
lxhcaicai <2778763221@qq.com>
2023-05-07 02:30:24 +0800

    [add] JudgeServerEntityService

---
+++ loj-backup/src/main/java/com/github/loj/dao/judge/JudgeServerEntityService.java
@@ -0,0 +1,11 @@
+package com.github.loj.dao.judge;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.loj.pojo.entity.judge.JudgeServer;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/7 2:20
+ */
+public interface JudgeServerEntityService extends IService<JudgeServer> {
+}
