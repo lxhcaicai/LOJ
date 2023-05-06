468b9034ff037e74360d3948221aca7eacc7a114
lxhcaicai <2778763221@qq.com>
2023-05-06 22:45:15 +0800

    [add] LOJAccessEnum

---
+++ loj-backup/src/main/java/com/github/loj/annotation/LOJAccessEnum.java
@@ -0,0 +1,42 @@
+package com.github.loj.annotation;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/6 22:42
+ */
+public enum LOJAccessEnum {
+    /**
+     * 公共讨论区
+     */
+    PUBLIC_DISCUSSION,
+
+    /**
+     * 团队讨论区
+     */
+    GROUP_DISCUSSION,
+
+    /**
+     * 比赛评论
+     */
+    CONTEST_COMMENT,
+
+    /**
+     * 公共评测
+     */
+    PUBLIC_JUDGE,
+
+    /**
+     * 团队评测
+     */
+    GROUP_JUDGE,
+
+    /**
+     * 比赛评测
+     */
+    CONTEST_JUDGE,
+
+    /**
+     * 隐藏非比赛提交详情的代码
+     */
+    HIDE_NON_CONTEST_SUBMISSION_CODE
+}
