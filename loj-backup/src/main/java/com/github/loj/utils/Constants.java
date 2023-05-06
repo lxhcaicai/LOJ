b64ab042de18ae0dddf5bf4c76e18fd745163a40
lxhcaicai <2778763221@qq.com>
2023-05-07 01:23:05 +0800

    [add] 等待判题的redis队列 Queue

--- loj-backup/src/main/java/com/github/loj/utils/Constants.java
+++ loj-backup/src/main/java/com/github/loj/utils/Constants.java
@@ -88,4 +88,25 @@ public class Constants {
             return columnName;
         }
     }
+
+    /**
+     * 等待判题的redis队列
+     */
+    public enum Queue {
+        CONTEST_JUDGE_WAITING("Contest_Waiting_Handle_Queue"),
+        GENERAL_JUDGE_WAITING("General_Waiting_Handle_Queue"),
+        TEST_JUDGE_WAITING("Test_Judge_Waiting_Handle_Queue"),
+        CONTEST_REMOTE_JUDGE_WAITING_HANDLE("Contest_Remote_Waiting_Handle_Queue"),
+        GENERAL_REMOTE_JUDGE_WAITING_HANDLE("General_Remote_Waiting_Handle_Queue");
+
+        private final String name;
+
+        private Queue(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
