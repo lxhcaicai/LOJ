bb71232349c7dd3a7ade44dd2389f53e814a4baf
lxhcaicai <2778763221@qq.com>
2023-05-07 18:07:24 +0800

    [add] GroupMember对象

---
+++ loj-api/src/main/java/com/github/loj/pojo/entity/group/GroupMember.java
@@ -0,0 +1,48 @@
+package com.github.loj.pojo.entity.group;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/7 18:03
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value = "GroupMember对象", description = "")
+public class GroupMember {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "团队id")
+    private Long gid;
+
+    @ApiModelProperty(value = "用户id")
+    private String uid;
+
+    @ApiModelProperty(value = "默认为1表示申请中，2为拒绝，3为普通成员，4为管理员，5为超级管理员")
+    private Integer auth;
+
+    @ApiModelProperty(value = "申请理由")
+    private String reason;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+}
