0b649c7dc0190243458aeb81519c20fa41b79006
lxhcaicai <2778763221@qq.com>
2023-05-10 23:52:05 +0800

    [add] ContestProblemEntityService

---
+++ loj-backup/src/main/java/com/github/loj/dao/contest/ContestProblemEntityService.java
@@ -0,0 +1,11 @@
+package com.github.loj.dao.contest;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.loj.pojo.entity.contest.ContestProblem;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/10 23:41
+ */
+public interface ContestProblemEntityService extends IService<ContestProblem> {
+}
---
+++ loj-backup/src/main/java/com/github/loj/dao/contest/impl/ContestProblemEntityServiceImpl.java
@@ -0,0 +1,13 @@
+package com.github.loj.dao.contest.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.loj.dao.contest.ContestProblemEntityService;
+import com.github.loj.mapper.ContestProblemMapper;
+import com.github.loj.pojo.entity.contest.ContestProblem;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/10 23:42
+ */
+public class ContestProblemEntityServiceImpl extends ServiceImpl<ContestProblemMapper, ContestProblem> implements ContestProblemEntityService {
+}
