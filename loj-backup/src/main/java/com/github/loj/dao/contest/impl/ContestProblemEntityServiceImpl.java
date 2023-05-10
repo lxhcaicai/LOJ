f1c3cc6b1e9c6bab0340b17d7c20a2ed43bf370c
lxhcaicai <2778763221@qq.com>
2023-05-11 00:03:36 +0800

    [add] @Service

--- loj-backup/src/main/java/com/github/loj/dao/contest/impl/ContestProblemEntityServiceImpl.java
+++ loj-backup/src/main/java/com/github/loj/dao/contest/impl/ContestProblemEntityServiceImpl.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.loj.dao.contest.ContestProblemEntityService;
 import com.github.loj.mapper.ContestProblemMapper;
 import com.github.loj.pojo.entity.contest.ContestProblem;
+import org.springframework.stereotype.Service;
 
 /**
  * @author lxhcaicai
  * @date 2023/5/10 23:42
  */
+@Service
 public class ContestProblemEntityServiceImpl extends ServiceImpl<ContestProblemMapper, ContestProblem> implements ContestProblemEntityService {
 }
