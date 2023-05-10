10a09a62fca57bc397789a28500a031415560da5
lxhcaicai <2778763221@qq.com>
2023-05-11 00:14:42 +0800

    [add] Training对象 训练题单实体

---
+++ loj-api/src/main/java/com/github/loj/pojo/entity/training/Training.java
@@ -0,0 +1,66 @@
+package com.github.loj.pojo.entity.training;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/11 0:10
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value = "Training对象", description = "训练题单实体")
+public class Training implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "主键id")
+    private Long id;
+
+    @ApiModelProperty(value = "训练题单标题")
+    private String title;
+
+    @ApiModelProperty(value = "训练题单简介")
+    private String description;
+
+    @ApiModelProperty(value = "训练题单创建者用户名")
+    private String author;
+
+    @ApiModelProperty(value = "训练题单权限类型：Public、Private")
+    private String auth;
+
+    @ApiModelProperty(value = "训练题单权限为Private时的密码")
+    private String privatePwd;
+
+    @ApiModelProperty(value = "是否可用")
+    private Boolean status;
+
+    @ApiModelProperty(value = "编号，升序排序")
+    @TableField("`rank`")
+    private Integer rank;
+
+    @ApiModelProperty(value = "是否为团队内的训练")
+    private Boolean isGroup;
+
+    @ApiModelProperty(value = "团队ID")
+    private Long gid;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+}
