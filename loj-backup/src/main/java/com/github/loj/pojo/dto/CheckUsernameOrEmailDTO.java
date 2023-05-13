192957281126dc3932880a8b50503f1ddffced42
lxhcaicai <2778763221@qq.com>
2023-05-13 21:58:44 +0800

    [add] 检查邮箱账号实体类

---
+++ loj-backup/src/main/java/com/github/loj/pojo/dto/CheckUsernameOrEmailDTO.java
@@ -0,0 +1,16 @@
+package com.github.loj.pojo.dto;
+
+import lombok.Data;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/13 21:58
+ */
+@Data
+public class CheckUsernameOrEmailDTO {
+
+    private String email;
+
+    private String username;
+
+}
