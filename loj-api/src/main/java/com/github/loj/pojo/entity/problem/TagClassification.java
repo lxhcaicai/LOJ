4ac476437659286cefd2b1638d7a5bd9b679fd66
lxhcaicai <2778763221@qq.com>
2023-05-15 21:09:52 +0800

    [add] TagClassification对象

---
+++ loj-api/src/main/java/com/github/loj/pojo/entity/problem/TagClassification.java
@@ -0,0 +1,44 @@
+package com.github.loj.pojo.entity.problem;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * @author lxhcaicai
+ * @date 2023/5/15 21:07
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="TagClassification对象", description="标签分类")
+public class TagClassification {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "标签分类名字")
+    private String name;
+
+    @ApiModelProperty(value = "标签分类所属oj")
+    private String oj;
+
+    @ApiModelProperty(value = "标签分类优先级 越小越高")
+    @TableField("`rank`")
+    private Integer rank;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+}
