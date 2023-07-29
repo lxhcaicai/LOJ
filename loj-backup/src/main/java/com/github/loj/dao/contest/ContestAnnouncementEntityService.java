a6326a0990c6e9f5e73ab6f3024b8f47c349c5b5
lxhcaicai <2778763221@qq.com>
2023-07-29 22:38:36 +0800

    move ContestAnnouncement.java

--- loj-backup/src/main/java/com/github/loj/dao/contest/ContestAnnouncementEntityService.java
+++ loj-backup/src/main/java/com/github/loj/dao/contest/ContestAnnouncementEntityService.java
@@ -1,7 +1,7 @@
 package com.github.loj.dao.contest;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.github.loj.pojo.entity.common.ContestAnnouncement;
+import com.github.loj.pojo.entity.contest.ContestAnnouncement;
 
 public interface ContestAnnouncementEntityService extends IService<ContestAnnouncement> {
 }
--- loj-backup/src/main/java/com/github/loj/dao/contest/impl/ContestAnnouncementEntityServiceImpl.java
+++ loj-backup/src/main/java/com/github/loj/dao/contest/impl/ContestAnnouncementEntityServiceImpl.java
@@ -3,7 +3,7 @@ package com.github.loj.dao.contest.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.loj.dao.contest.ContestAnnouncementEntityService;
 import com.github.loj.mapper.ContestAnnouncementMapper;
-import com.github.loj.pojo.entity.common.ContestAnnouncement;
+import com.github.loj.pojo.entity.contest.ContestAnnouncement;
 import org.springframework.stereotype.Service;
 
 @Service
