2bd9234c578bce232e7877cfe17c2e5f1646e93b
lxhcaicai <2778763221@qq.com>
2023-09-12 22:50:03 +0800

    fix admin problem

--- loj-backup/src/main/java/com/github/loj/controller/admin/AdminProblemController.java
+++ loj-backup/src/main/java/com/github/loj/controller/admin/AdminProblemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/amin/problem")
+@RequestMapping("/api/admin/problem")
 public class AdminProblemController {
 
     @Autowired
