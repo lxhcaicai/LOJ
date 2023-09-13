66d6234db272afb6fcf4ba65d685fe673e948c8e
lxhcaicai <2778763221@qq.com>
2023-09-14 03:04:23 +0800

    fix backup module package

--- loj-backup/src/main/java/com/github/loj/mapper/ProblemLanguageMapper.java
+++ loj-backup/src/main/java/com/github/loj/mapper/ProblemLanguageMapper.java
@@ -2,10 +2,10 @@ package com.github.loj.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.loj.pojo.entity.problem.ProblemLanguage;
-import com.sun.org.glassfish.gmbal.ManagedObject;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-@ManagedObject
+@Mapper
 @Repository
 public interface ProblemLanguageMapper extends BaseMapper<ProblemLanguage> {
 }
